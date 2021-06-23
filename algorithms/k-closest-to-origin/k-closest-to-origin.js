40de00e012bec34a3b1d3307a54d3b8e98c1862d
Ananth <ananth.kumbla@gmail.com>
2021-06-23 11:04:21 +0530

    k-closest problem in leet code

---
+++ algorithms/k-closest-to-origin/k-closest-to-origin.js
@@ -0,0 +1,27 @@
+/**
+ * Given an array of points
+ *  where points[i] = [xi, yi] represents a point on the X-Y plane 
+ *  and an integer k, return the k closest points to the origin (0, 0).
+   The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+   You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in)
+ */
+const getDistance = (a, b) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+const kClosesToOrigin = (arr, k) => {
+  //define the formulae
+  arr.sort(([a, b], [c, d]) => {
+    return getDistance(a, b) - getDistance(c, d);
+  });
+
+  return arr.slice(0, k);
+};
+
+const kAnswer = kClosesToOrigin(
+  [
+    [1, 3],
+    [-2, 2],
+  ],
+  1
+);
+
+console.log("ans", kAnswer);
